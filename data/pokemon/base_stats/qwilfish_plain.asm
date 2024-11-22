241f86cbc0657e9ffd724f19b4f3c0ca75688984
CesareDBass <garantonortegace08@gmail.com>
2024-11-22 02:19:36 -0500

    Update qwilfish_plain.asm

--- data/pokemon/base_stats/qwilfish_plain.asm
+++ data/pokemon/base_stats/qwilfish_plain.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	db  65,  95,  85,  85,  55,  55 ; 440 BST
 	;   hp  atk  def  spe  sat  sdf
 else
-	db  65, 100, 100,  85,  75,  55 ; 480 BST
+	db  65, 100, 100,  85,  55,  55 ; 480 BST
 	;   hp  atk  def  spe  sat  sdf
 endc
 
